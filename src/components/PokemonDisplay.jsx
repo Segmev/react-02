a10a6bd414d31329a735f771f1295b34570b9a0d
Stéphane K <steffkarraz@hotmail.com>
2020-12-17 23:24:09 +0100

    with timeout solution

--- src/components/PokemonDisplay.jsx
+++ src/components/PokemonDisplay.jsx
@@ -1,27 +1,34 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 export default function PokemonDisplay({ pokemonUrl }) {
-  let [pokemonData, setPokemonData] = useState({})
-  console.log(pokemonData)
+  let [pokemonData, setPokemonData] = useState({});
+  console.log(pokemonData);
 
   useEffect(() => {
+    let lastCall = true;
     const fetchData = () => {
       if (typeof pokemonUrl === "string") {
         fetch(pokemonUrl)
           .then((response) => response.json())
-          .then((data) => setPokemonData(data))
-          .catch((e) => console.error(e))
+          .then((data) => lastCall && setPokemonData(data))
+          .catch((e) => console.error(e));
       }
     };
 
     setTimeout(fetchData, 10000 * Math.random());
-  }, [pokemonUrl])
+    return () => {
+      lastCall = false;
+    };
+  }, [pokemonUrl]);
 
   if (!pokemonData.id) {
-    return (<></>)
+    return <></>;
   }
-  return (<>
-    <div>id: {pokemonData.id}</div>
-    <img alt="pokemon figure" src={pokemonData.sprites?.front_default} />
-  </>)
+  return (
+    <>
+      <div>id: {pokemonData.id}</div>
+      <div>name: {pokemonData.name}</div>
+      <img alt="pokemon figure" src={pokemonData.sprites?.front_default} />
+    </>
+  );
 }
